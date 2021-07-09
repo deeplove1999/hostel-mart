2c03a4bf589a0dbf8e579f0ba780b6d5a39ed337
lovedeep <08lovedeep@gmail.com>
2021-07-09 10:23:01 +0530

    added remove and add button

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,7 +1,3 @@
-/* Utility functions allow us to keep our files 
-clean and organize functions that we may need 
-in multiple files in one location */
-
 export const addItemToCart = (cartItems, cartItemToAdd) => {
   const existingCartItem = cartItems.find(
     cartItem => cartItem.id === cartItemToAdd.id
@@ -14,5 +10,22 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
         : cartItem
     );
   }
+
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
 };
